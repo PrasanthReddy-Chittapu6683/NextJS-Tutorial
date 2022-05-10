c0a56cfc0134b35d7a9d868a0b8ee872d6614c5b
Prasanth Reddy CV <chittapuvenkata.p@mphasis.com>
2022-05-11 04:25:19 +0530

    Dynamic & Fixed Routing

---
+++ nextjs-concepts/pages/product/electronics.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+
+// Fixed path rendering
+const electronics = () => {
+    return (
+        <h3>Landing to Electronic</h3>
+    )
+}
+
+export default electronics
---
+++ nextjs-concepts/pages/product/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const product = () => {
+    return (
+        <>
+            <h3>Product - 1</h3>
+            <h3>Product - 2</h3>
+            <h3>Product - 3</h3>
+        </>
+    )
+}
+
+export default product
