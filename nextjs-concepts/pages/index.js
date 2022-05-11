c1cbc2fc83f596709f0a44ed5d2203732608bd10
Prasanth Reddy CV <chittapuvenkata.p@mphasis.com>
2022-05-11 05:42:29 +0530

    10-linked-component-navigations

--- nextjs-concepts/pages/index.js
+++ nextjs-concepts/pages/index.js
@@ -1,5 +1,26 @@
+import Link from 'next/link'
+
 function Home(params) {
-    return <h1>Home Page</h1>
+    return (<div>
+        <h1>Home Page</h1>
+        <ul>
+            <li>
+                <Link href='/blog'>
+                    <a> Blogs</a>
+                </Link>
+            </li>
+            <li>
+                <Link href='/product'>
+                    <a> Products</a>
+                </Link>
+            </li>
+        </ul>
+
+
+    </div>
+    )
+
+
 }
 
 
--- nextjs-concepts/pages/product/index.js
+++ nextjs-concepts/pages/product/index.js
@@ -1,13 +1,37 @@
-import React from 'react'
+import Link from 'next/link'
 
-const product = () => {
+const productList = ({ productId = 100 }) => {
+
     return (
         <>
-            <h3>Product - 1</h3>
-            <h3>Product - 2</h3>
-            <h3>Product - 3</h3>
+            <ul>
+                <li>
+                    <Link href='/'>
+                        <a>Back to Home</a>
+                    </Link>
+                </li>
+            </ul>
+            <h3>
+                <Link href='/product/1'>
+                    <a>Product - 1</a>
+                </Link>
+            </h3>
+            <h3>
+                <Link href='/product/2'>
+                    <a>Product - 2</a>
+                </Link></h3>
+            <h3>
+                <Link href='/product/3' replace>
+                    <a>Product - 3 - (using replace prop which redirects to home page)</a>
+                </Link>
+            </h3>
+            <h3>
+                <Link href={`/product/${productId}`}>
+                    <a>Product - {productId}</a>
+                </Link>
+            </h3>
         </>
     )
 }
 
-export default product
+export default productList
