f688d7a9da42cf88fda5316c4847cef491377ce9
Prasanth Reddy CV <chittapuvenkata.p@mphasis.com>
2022-05-11 05:57:19 +0530

    11-Navigating-Programmatically

--- nextjs-concepts/pages/index.js
+++ nextjs-concepts/pages/index.js
@@ -1,8 +1,17 @@
 import Link from 'next/link'
+import { useRouter } from 'next/router'
 
 function Home(params) {
+    const router = useRouter();
+
+    const handleClick = () => {
+        router.push('/product')
+    }
     return (<div>
         <h1>Home Page</h1>
+        <button onClick={handleClick}>
+            Place Orders (Navigate programatically on button click)
+        </button>
         <ul>
             <li>
                 <Link href='/blog'>
